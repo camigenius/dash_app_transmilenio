82728b9e7dc3287b76e8ca904d83ee1f27d5d320
camilo franco <camilofinanzas@gmail.com>
2022-12-09 07:46:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from dash import Dash, dcc, html, Input, Output
+from plotly.express import data
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_pickle('my_dataframe.pickle')
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    
+    html.H1('UN DIA CUALQUIERA EN TRANSMILENIO!!'),
+    dcc.Graph(id="graph"),    
+    dcc.Dropdown(df.Linea.unique(), id='dropdown',placeholder="Seleccione una estacón"),
+    html.H4('Elaboradpo por camilo franco'),
+     
+    
+    
+    #html.Div(id='pandas-output-container-1'),
+
+    
+])
+
+
+@app.callback(
+    Output('graph','figure'),
+    Input('dropdown', 'value')
+)
+def update_output(value):
+    portal = df[df['Linea']==value]
+    estaciones=portal['Estacion_Parada'].value_counts()
+    fig = px.bar(estaciones, x=estaciones.index, y=estaciones)
+    #fig.show()  
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
